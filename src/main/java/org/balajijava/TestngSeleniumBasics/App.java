78d0dd92dca62129484eed4ade61c49ab40ed9d3
bvisvanathan <balaji.visvanathan@gmail.com>
2020-10-04 14:27:56 +0530

    TestNG Selenium Java Maven Practice Application Initial Version 1.0

---
+++ src/main/java/org/balajijava/TestngSeleniumBasics/App.java
@@ -0,0 +1,21 @@
+package org.balajijava.TestngSeleniumBasics;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+   /* public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }*/
+    
+    @Test
+	public void test1() {
+		
+		System.out.println( "Hello World!" );    	
+    }
+}
