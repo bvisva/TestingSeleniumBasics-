9d9a442139308f04571674e94dc82e75468503b6
bvisvanathan <balaji.visvanathan@gmail.com>
2020-10-10 14:51:30 +0530

    Full page screen shot shutterbug with scroll and javascript executer added version 1.1

--- src/main/java/org/balajijava/TestngSeleniumBasics/App.java
+++ src/main/java/org/balajijava/TestngSeleniumBasics/App.java
@@ -1,6 +1,11 @@
 package org.balajijava.TestngSeleniumBasics;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import com.assertthat.selenium_shutterbug.core.Capture;
+import com.assertthat.selenium_shutterbug.core.Shutterbug;
 
 /**
  * Hello world!
@@ -16,6 +21,15 @@ public class App
     @Test
 	public void test1() {
 		
-		System.out.println( "Hello World!" );    	
+		System.out.println( "Hello World!" );
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\DELL\\eclipse-workspace\\BrowserDrivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();		
+		driver.get("https://www.msn.com/en-in/");
+		driver.manage().window().maximize();
+		System.out.println( "URL success" );		
+		 Shutterbug.shootPage(driver,Capture.FULL_SCROLL,500,true).withName("HotmailFullPageScreenshot").save("D:\\Images\\");
+	       driver.quit();
+	       System.out.println( "screen shot success" );
     }
 }
